9627f16d63b7adf5b91b39a8cc04ef1f87edb458
xujiang <realjiangxu@gmail.com>
2020-06-09 00:31:24 +0800

    [jdk] fix jdk install faild

--- bigtop_toolchain/manifests/jdk.pp
+++ bigtop_toolchain/manifests/jdk.pp
@@ -16,26 +16,6 @@
 class bigtop_toolchain::jdk {
   case $::operatingsystem {
     /Debian/: {
-      require apt
-      unless $operatingsystemmajrelease > "8" {
-         # we pin openjdk-8-* and ca-certificates-java to backports
-         require apt::backports
-
-         apt::pin { 'backports_jdk':
-            packages => 'openjdk-8-*',
-            priority => 500,
-            release  => 'jessie-backports',
-         } ->
-         apt::pin { 'backports_ca':
-            packages => 'ca-certificates-java',
-            priority => 500,
-            release  => 'jessie-backports',
-         } ->
-         exec {'own_update':
-            command => '/usr/bin/apt-get update'
-         } -> Package['openjdk-8-jdk']
-      }
-
       package { 'openjdk-8-jdk' :
         ensure => present,
       }
@@ -45,35 +25,12 @@ class bigtop_toolchain::jdk {
 
       package { 'openjdk-8-jdk' :
         ensure  => present,
-        # needed for 14.04 
-        require => [ Apt::Ppa[ 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu'], Class['apt::update'] ]
       }
-
-      apt::key { 'openjdk-ppa':
-        id     => 'eb9b1d8886f44e2a',
-        server => 'keyserver.ubuntu.com'
-      }  ->
-      apt::ppa { 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu':  }
     }
     /(CentOS|Amazon|Fedora)/: {
-      # BIGTOP-2900: Crunch build failed because of OS OOM killer on OpenJDK 1.8.0-144
-      # Here we pin other version of JDK for CentOS 7 and Fedora to workaround this problem
-      if ($::operatingsystem == "CentOS" and $operatingsystemmajrelease == "7") {
-        $jdk = [
-          'java-1.8.0-openjdk-headless-1.8.0.131',
-          'java-1.8.0-openjdk-devel-1.8.0.131'
-        ]
-      }
-      elsif ($::operatingsystem == "Fedora") {
-        $jdk = 'java-1.8.0-openjdk-devel-1:1.8.0.111'
-      }
-      else {
-        $jdk = 'java-1.8.0-openjdk-devel'
-      }
-      package { $jdk :
+      package { 'java-1.8.0-openjdk-devel' :
         ensure => present
       }
-
       if ($::operatingsystem == "Fedora") {
         file { '/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/security/cacerts':
           ensure => 'link',
