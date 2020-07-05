a3ee40b944dce7822fb43ac50a31f2d5c813e5bd
xujiang <realjiangxu@gmail.com>
2020-07-05 23:54:58 +0800

    fix jdp to anyscale

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run --rm -v /data/gitlab/jdp:/ws -v ~/.m2:/root/.m2 -v ~/.npm:/root/.npm -v ~/.cache:/root/.cache -v ~/.gradle:/root/.gradle -v ~/.ivy2:/root/.ivy2 -v /tmp:/tmp  --workdir /ws topflow/slaves:trunk-centos-7 bash -l -c "cd jdp-package ; ./gradlew clean clickhouse-rpm"
+docker run --rm -v /data/gitlab/jdp:/ws -v ~/.m2:/root/.m2 -v ~/.npm:/root/.npm -v ~/.cache:/root/.cache -v ~/.gradle:/root/.gradle -v ~/.ivy2:/root/.ivy2 -v /tmp:/tmp  --workdir /ws topflow/slaves:trunk-centos-7 bash -l -c "cd anyscale-package ; ./gradlew clean clickhouse-rpm"
